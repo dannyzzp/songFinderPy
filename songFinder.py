3904e1c9a383881e47651ecc2417b90b1d85f13e
Zhanpeng Danny Zhang <dannyzzp26276819@gmail.com>
2025-02-03 00:40:47 -0800

    Update songFinder.py

--- songFinder.py
+++ songFinder.py
@@ -11,11 +11,12 @@ Original file is located at
 # drive.mount('/content/drive')
 import sys
 print(sys.executable)
-import librosa
+import os
 import numpy as np
 import pandas as pd
 import math
 import pickle
+import librosa
 
 def cosineSim(a,b):
   return(np.dot(a,b)/(np.linalg.norm(a) * np.linalg.norm(b)))
