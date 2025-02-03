f407820f8d98bd8eed940255d8d241eb30eee47b
Zhanpeng Danny Zhang <dannyzzp26276819@gmail.com>
2025-02-02 23:31:18 -0800

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import streamlit as st
 import subprocess
 import os
 import pygame
+import librosa
 
 # Set the working directory
 #os.chdir('C:\\Users\\danny\\onedrive\\desktop\\songfinder')
@@ -23,8 +24,8 @@ def run_song_finder():
     st.write(os.path.isfile(os.listdir(os.getcwd())[16]))
     st.write(os.listdir(os.getcwd()))
     result = subprocess.run(
-        #["python", "songFinder.py"],
-        ["python", os.listdir(os.getcwd())[16]],
+        ["python", "songFinder.py"],
+        #["python", os.listdir(os.getcwd())[16]],
         capture_output=False,
         text=False
     )
